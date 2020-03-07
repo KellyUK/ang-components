26f7b582255e8d6198c75fbf50c159eb418acfd1
Kelly Davidson <kellyadavidsonuk@gmail.com>
2020-03-07 16:46:09 +0000

    add table keys

--- src/app/collections/collections-home/collections-home.component.ts
+++ src/app/collections/collections-home/collections-home.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-collections-home',
-  templateUrl: './collections-home.component.html',
-  styleUrls: ['./collections-home.component.css']
+  selector: "app-collections-home",
+  templateUrl: "./collections-home.component.html",
+  styleUrls: ["./collections-home.component.css"]
 })
 export class CollectionsHomeComponent implements OnInit {
+  data: [
+    {
+      name: "james";
+      age: 25;
+      job: "baker";
+    },
+    {
+      name: "Jill";
+      age: 25;
+      job: "Engineer";
+    },
+    {
+      name: "bill";
+      age: 25;
+      job: "dog walker";
+    }
+  ];
 
-  constructor() { }
+  headers: [
+    { key: "name"; label: "Name" },
+    { key: "age"; label: "Age" },
+    { key: "job"; label: "Job" }
+  ];
 
-  ngOnInit(): void {
-  }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
