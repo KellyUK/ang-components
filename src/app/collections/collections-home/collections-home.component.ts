87eae5c11a141fda86887ca8145349442a18632a
Kelly Davidson <kellyadavidsonuk@gmail.com>
2020-03-07 19:50:50 +0000

    add table headers

--- src/app/collections/collections-home/collections-home.component.ts
+++ src/app/collections/collections-home/collections-home.component.ts
@@ -1,36 +1,25 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: "app-collections-home",
-  templateUrl: "./collections-home.component.html",
-  styleUrls: ["./collections-home.component.css"]
+  selector: 'app-collections-home',
+  templateUrl: './collections-home.component.html',
+  styleUrls: ['./collections-home.component.css']
 })
 export class CollectionsHomeComponent implements OnInit {
-  data: [
-    {
-      name: "james";
-      age: 25;
-      job: "baker";
-    },
-    {
-      name: "Jill";
-      age: 25;
-      job: "Engineer";
-    },
-    {
-      name: "bill";
-      age: 25;
-      job: "dog walker";
-    }
+  data = [
+    { name: 'James', age: 24, job: 'Designer', employed: true },
+    { name: 'Jill', age: 26, job: 'Engineer', employed: false },
+    { name: 'Elyse', age: 25, job: 'Engineer', employed: true }
   ];
-
-  headers: [
-    { key: "name"; label: "Name" },
-    { key: "age"; label: "Age" },
-    { key: "job"; label: "Job" }
+  headers = [
+    { key: 'employed', label: 'Has a Job?' },
+    { key: 'name', label: 'Name' },
+    { key: 'age', label: 'Age' },
+    { key: 'job', label: 'Job' }
   ];
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit() {}
 }
+
