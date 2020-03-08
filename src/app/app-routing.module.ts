46dcf9da8d3cff18dda556558cebeffb4710931e
Kelly Davidson <kellyadavidsonuk@gmail.com>
2020-03-08 16:55:01 +0000

    plug in modal component and mods home to router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,25 +1,29 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { NotFoundComponent } from './not-found/not-found.component';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { HomeComponent } from "./home/home.component";
+import { NotFoundComponent } from "./not-found/not-found.component";
 
 const routes: Routes = [
   {
-    path: 'collections',
+    path: "collections",
     loadChildren: () =>
-      import('./collections/collections.module').then(m => m.CollectionsModule)
+      import("./collections/collections.module").then(m => m.CollectionsModule)
   },
   {
-    path: 'elements',
+    path: "elements",
     loadChildren: () =>
-      import('./elements/elements.module').then(m => m.ElementsModule)
+      import("./elements/elements.module").then(m => m.ElementsModule)
   },
   {
-    path: 'views',
-    loadChildren: () => import('./views/views.module').then(m => m.ViewsModule)
+    path: "views",
+    loadChildren: () => import("./views/views.module").then(m => m.ViewsModule)
   },
-  { path: '', component: HomeComponent },
-  { path: '**', component: NotFoundComponent }
+  {
+    path: "mods",
+    loadChildren: () => import("./mods/mods.module").then(m => m.ModsModule)
+  },
+  { path: "", component: HomeComponent },
+  { path: "**", component: NotFoundComponent }
 ];
 
 @NgModule({
