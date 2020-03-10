22718e5a1d3f61d56371c91b867cd80b09d8f2cc
Kelly Davidson <kellyadavidsonuk@gmail.com>
2020-03-10 13:07:15 +0000

    ngOnDestroy to remove modal

--- src/app/mods/modal/modal.component.ts
+++ src/app/mods/modal/modal.component.ts
@@ -1,16 +1,19 @@
-import { Component, OnInit, ElementRef } from '@angular/core';
+import { Component, OnInit, ElementRef } from "@angular/core";
 
 @Component({
-  selector: 'app-modal',
-  templateUrl: './modal.component.html',
-  styleUrls: ['./modal.component.css']
+  selector: "app-modal",
+  templateUrl: "./modal.component.html",
+  styleUrls: ["./modal.component.css"]
 })
 export class ModalComponent implements OnInit {
-
-  constructor(private el: ElementRef) { }
+  constructor(private el: ElementRef) {}
 
   ngOnInit(): void {
     document.body.appendChild(this.el.nativeElement);
   }
-
+  ngOnDestroy(): void {
+    //Called once, before the instance is destroyed.
+    //Add 'implements OnDestroy' to the class.
+    this.el.nativeElement.remove();
+  }
 }
